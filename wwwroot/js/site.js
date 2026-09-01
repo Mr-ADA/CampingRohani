9422b99f07e903c8efb383e038337aea83976cce
Allen Dylan Antony <allendylanantony0312@gmail.com>
2026-09-01 16:45:24 +0700

    initial commit project CampingRohani

---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//toggle class active
+const navbarNav = document.querySelector("div.navbar-nav");
+
+//ketika hamburger menu di click
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", (e) => {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
